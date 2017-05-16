936823e2f420fecf1add04032982e718cea1ed8e
Anthony <AnthonyL22@gmail.com>
2017-05-16 07:14:25 -0700

    Modified static input

--- src/test/java/com/mycompany/myproject/automation/tests/web/BasicTest.java
+++ src/test/java/com/mycompany/myproject/automation/tests/web/BasicTest.java
@@ -1,17 +1,17 @@
 package com.mycompany.myproject.automation.tests.web;
 
 import com.mycompany.myproject.automation.data.Constants;
-import com.mycompany.myproject.automation.data.provider.SearchDataProvider;
 import com.mycompany.myproject.automation.frameworksupport.Groups;
 import com.mycompany.myproject.automation.frameworksupport.MyApplicationTestCase;
 import com.pwc.core.framework.annotations.MaxRetryCount;
 import com.pwc.core.framework.listeners.Retry;
-import org.apache.xpath.operations.String;
 import org.testng.annotations.Test;
 
 import static com.pwc.logging.service.LoggerService.*;
 
 public class BasicTest extends MyApplicationTestCase {
+
+    public static final String SEARCH_TEXT = "pacificwebconsulting";
 
     @Override
     public void beforeMethod() {
@@ -22,8 +22,8 @@ public class BasicTest extends MyApplicationTestCase {
     }
 
     @MaxRetryCount(1)
-    @Test(retryAnalyzer = Retry.class, groups = {Groups.ACCEPTANCE_TEST}, dataProvider = "basicSearchText", dataProviderClass = SearchDataProvider.class)
-    public void testBasic(String searchText) {
+    @Test(retryAnalyzer = Retry.class, groups = {Groups.ACCEPTANCE_TEST})
+    public void testBasic() {
 
         FEATURE("Feature Under Test");
         SCENARIO("Scenario Being Tested Here");
@@ -32,14 +32,14 @@ public class BasicTest extends MyApplicationTestCase {
         webElementVisible(Constants.LOGO_IMAGE);
 
         WHEN("I do something");
-        webAction(Constants.QUERY_INPUT, searchText);
+        webAction(Constants.QUERY_INPUT, SEARCH_TEXT);
 
         THEN("Something happens as expected");
         webAction(Constants.SEARCH_BUTTON);
         webElementExists(Constants.CORE_ANCHOR);
 
         webAction(Constants.RUNNER_ANCHOR);
-        webElementTextNotEquals(Constants.QUERY_INPUT, "");
+        webElementTextEquals(Constants.QUERY_INPUT, "a:\"runner-microservice\"");
 
     }
 
