8dcd6105ac5efed2e33a68cda064b3fb2492fe36
c-anlomb <c_anlomb@qualcomm.com>
2017-11-06 11:30:26 -0800

    Converted from Constants interface 'anti-pattern' to more appropriate final pattern for xpath and data constants.

--- src/test/java/com/mycompany/myproject/automation/data/Constants.java
+++ src/test/java/com/mycompany/myproject/automation/data/Constants.java
@@ -1,25 +1,28 @@
 package com.mycompany.myproject.automation.data;
 
-public interface Constants {
+public final class Constants {
+
+    private Constants() {
+    }
 
     // Application Defaults
-    byte[] SECURITY_KEY = {0x74, 0x68, 0x69, 0x73, 0x49, 0x73, 0x41, 0x53, 0x65, 0x63, 0x72, 0x65, 0x74, 0x4b, 0x65, 0x79};
-    int MAX_WEB_SERVICE_RESPONSE_TIME = 3000;
+    public static final byte[] SECURITY_KEY = {0x74, 0x68, 0x69, 0x73, 0x49, 0x73, 0x41, 0x53, 0x65, 0x63, 0x72, 0x65, 0x74, 0x4b, 0x65, 0x79};
+    public static final int MAX_WEB_SERVICE_RESPONSE_TIME = 3000;
 
     // IMG Elements
-    String LOGO_IMAGE = "The Central Repository";
+    public static final String LOGO_IMAGE = "The Central Repository";
 
     // ANCHOR Elements
-    String CORE_ANCHOR = "core-microservice";
-    String RUNNER_ANCHOR = "runner-microservice";
+    public static final String CORE_ANCHOR = "core-microservice";
+    public static final String RUNNER_ANCHOR = "runner-microservice";
 
     // HEADING Elements
 
     // INPUT Elements
-    String QUERY_INPUT = "query";
+    public static final String QUERY_INPUT = "query";
 
     // BUTTON Elements
-    String SEARCH_BUTTON = "Search";
+    public static final String SEARCH_BUTTON = "Search";
 
     // SELECT Elements
 
--- src/test/java/com/mycompany/myproject/automation/data/Data.java
+++ src/test/java/com/mycompany/myproject/automation/data/Data.java
@@ -2,8 +2,11 @@ package com.mycompany.myproject.automation.data;
 
 import com.pwc.core.framework.data.Credentials;
 
-public interface Data {
+public final class Data {
 
-    Credentials DEFAULT_USER_CREDENTIALS = new Credentials("9S/FX+f8hokEKHpRpcKIhID+94tbkZZb+l8DiLljRwo=", "XzIYvpekgl3myKR2IWo01MBczOkSKcfSL+UucSOmG58=");
+    private Data() {
+    }
+
+    public static final Credentials DEFAULT_USER_CREDENTIALS = new Credentials("9S/FX+f8hokEKHpRpcKIhID+94tbkZZb+l8DiLljRwo=", "XzIYvpekgl3myKR2IWo01MBczOkSKcfSL+UucSOmG58=");
 
 }
