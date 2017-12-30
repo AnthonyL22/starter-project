47a32eb5f80680a21703ac28ff16ca43fd8cd988
c-anlomb <c_anlomb@qualcomm.com>
2017-12-30 07:38:40 -0800

    Added new random list methods

--- src/test/java/com/mycompany/myproject/automation/frameworksupport/MyApplicationTestCase.java
+++ src/test/java/com/mycompany/myproject/automation/frameworksupport/MyApplicationTestCase.java
@@ -2,16 +2,27 @@ package com.mycompany.myproject.automation.frameworksupport;
 
 import com.mycompany.myproject.automation.data.Constants;
 import com.mycompany.myproject.automation.data.Data;
+import com.pwc.core.framework.FrameworkConstants;
 import com.pwc.core.framework.WebTestCase;
 import com.pwc.core.framework.command.WebServiceCommand;
 import com.pwc.core.framework.data.Credentials;
+import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
+
+import static com.pwc.assertion.AssertService.assertFail;
+import static com.pwc.logging.service.LoggerService.LOG;
 
 public abstract class MyApplicationTestCase extends WebTestCase {
 
@@ -32,6 +43,18 @@ public abstract class MyApplicationTestCase extends WebTestCase {
 
         webAction(Constants.LOGO_IMAGE);
 
+    }
+
+    @BeforeMethod(firstTimeOnly = true)
+    public void preserveProduction(Method m) {
+        if (StringUtils.containsIgnoreCase(System.getProperty(FrameworkConstants.AUTOMATION_TEST_ENVIRONMENT), "prod")) {
+            Test currentTest = m.getAnnotation(Test.class);
+            if (!Arrays.asList(currentTest.groups()).contains(Groups.ACCEPTANCE_TEST) || Arrays.asList(currentTest.groups()).contains(Groups.IN_PROGRESS_TEST)) {
+                assertFail("PREVENTING EXECUTION OF REGRESSION TEST='%s' IN PRODUCTION", m.getName());
+                tearDownClass();
+                System.exit(1);
+            }
+        }
     }
 
     @AfterClass(alwaysRun = true)
@@ -69,6 +92,69 @@ public abstract class MyApplicationTestCase extends WebTestCase {
      */
     protected Object webServiceAction(final WebServiceCommand command, final HashMap<String, Object> parameterMap) {
         return super.webServiceAction(command, null, parameterMap);
+    }
+
+    /**
+     * Get a random snippet of a given String from the 0 index to a random location in the given <code>String</code>
+     *
+     * @param displayedColumnValue base value to get a snippet of
+     * @return sub-string of original value
+     */
+    protected String getRandomBeginningSubString(String displayedColumnValue) {
+        int randomEnd = 1 + (int) (Math.random() * displayedColumnValue.length() - 1);
+        return StringUtils.substring(displayedColumnValue, 0, randomEnd);
+    }
+
+    /**
+     * Get a random fragment of a given sentence of words
+     *
+     * @param displayedColumnValue base value to get a snippet or sentence
+     * @return sub-string of original value
+     */
+    protected String getRandomSubSentenceFragment(String displayedColumnValue) {
+
+        List<String> splitResult = new ArrayList<>(Arrays.asList(StringUtils.split(displayedColumnValue)));
+        Random random = new Random();
+        int randomEnd = 1 + (int) (random.nextDouble() * splitResult.size() - 1);
+
+        String result = splitResult.get(0);
+        for (int i = 1; i < randomEnd; i++) {
+            result = result + " " + splitResult.get(i);
+        }
+        return result;
+    }
+
+    /**
+     * Get a random snippet of a given String from a random location in the given <code>String</code> to the end
+     *
+     * @param displayedColumnValue base value to get a snippet of
+     * @return sub-string of original value
+     */
+    protected String getRandomEndingSubString(String displayedColumnValue) {
+        Random random = new Random();
+        int randomBegin = 1 + (int) (random.nextDouble() * displayedColumnValue.length() - 1);
+        return StringUtils.substring(displayedColumnValue, randomBegin, displayedColumnValue.length());
+    }
+
+    /**
+     * Get a random sub-list of a given values from a random set in the given <code>List</code> to the end
+     *
+     * @param listValues base value to get a snippet of
+     * @return sub-string of original value
+     */
+    protected List getRandomList(List listValues) {
+        try {
+            Random random = new Random();
+            int randomBegin = 1 + (int) (random.nextDouble() * listValues.size() - 1);
+            if ((listValues.subList(0, randomBegin)).isEmpty()) {
+                return listValues;
+            } else {
+                return listValues.subList(0, randomBegin);
+            }
+        } catch (Exception e) {
+            LOG(true, "Unable to get random list due to reason='%s'", e);
+            return listValues;
+        }
     }
 
     public boolean isHeadlessMode() {
